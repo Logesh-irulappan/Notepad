b40f2606d52f34654737846819c03befc4170cb4
Logesh <146101687+Logesh-irulappan@users.noreply.github.com>
2024-12-13 08:47:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "Notepad",
-    password: "Logesh@0414",
+    password: "*******",
     port: "5432",
 });
 db.connect();
